3f9adefc95023feba1137d186618cda56edb1725
NofarZiv <nofarziv13@gmail.com>
2024-02-14 12:04:32 -0330

    added a keyboard command using stdin.

--- play.js
+++ play.js
@@ -1,3 +1,18 @@
 const {host, port} = require('./client');
 
 
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);// your code here
+  return stdin;
+};
+
+const handleUserInput = function (key) {
+if (key === '\u0003') {
+  process.exit();
+}}
+
+setupInput();
