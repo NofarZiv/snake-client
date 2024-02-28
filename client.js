029029667b3643c7b87493f8726e2498bae8842c
NofarZiv <nofarziv13@gmail.com>
2024-02-28 11:55:35 -0330

    added a README file.

--- client.js
+++ client.js
@@ -2,24 +2,24 @@ const net = require("net");
 const { IP, PORT } = require("./constants");
 
 // establishes a connection with the game server
-const connect = function () {
+const connect = function() {
   const conn = net.createConnection({
     host: IP,
     port: PORT,
   });
 
   conn.on('connect', () => {
-    console.log("Successfully connected to game server")
+    console.log("Successfully connected to game server");
   });
   conn.on("data", (data) => {
-    console.log("Server says: ", data)
+    console.log("Server says: ", data);
   });
 
-   conn.on("connect", () => {
-   conn.write("Name: NZ");
- });
+  conn.on("connect", () => {
+    conn.write("Name: NZ");
+  });
 
-    conn.write("Say: hi");
+  conn.write("Say: hi");
 
  
   // interpret incoming data as text
--- input.js
+++ input.js
@@ -1,6 +1,6 @@
 let connection;
 
-const setupInput = function (conn) {
+const setupInput = function(conn) {
   connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
@@ -10,22 +10,26 @@ const setupInput = function (conn) {
   return stdin;
 };
 
-const handleUserInput = function (key) {
-if (key === '\u0003') {
-  process.exit();
-}
-if (key === 'w') {
-    connection.write("Move: up"); }
+const handleUserInput = function(key) {
+  if (key === '\u0003') {
+    process.exit();
+  }
+  if (key === 'w') {
+    connection.write("Move: up");
+  }
 
-if (key === 'a') {
-    connection.write("Move: left") }
+  if (key === 'a') {
+    connection.write("Move: left");
+  }
 
-if (key === 's') {
-    connection.write("Move: down"); }
+  if (key === 's') {
+    connection.write("Move: down");
+  }
 
-if (key === 'd') {
-    connection.write("Move: right"); }
-}
+  if (key === 'd') {
+    connection.write("Move: right");
+  }
+};
 
 
 
